a207cc898679095605ef096af1447ed4ca031ca2
tomersamara <tomersamara@mail.tau.ac.il>
2017-05-24 20:33:12 +0300

    some changes on the sniffer

--- s_sniffer.py
+++ s_sniffer.py
@@ -1,4 +1,4 @@
-import scapy.all as S
+from scapy.all import *
 
 FILE_LOGGER = "./Ssniffer_logger.log"
 
@@ -24,23 +24,49 @@ class S_sniffer(object):
 
     # This function is to inform that we start using our
     # Ssniffer to check what is good and bad
-    def __init__(self, ourIP):
+    def __init__(self, our_ip):
         self.file = open(FILE_LOGGER, "w")
-        self.ourIP = ourIP
+        self.our_ip = our_ip
         self.sessions = {}
-        self.current_packet
+        self.current_packet = None
         print("Hello everyone this is the Ssniffer")
-        print("This sinffer is much better then any other sniffer beacuse:")
-        print("These sniffer give you a warning if you are trying to reach a malware")
+        print("This sniffer is much better then any other sniffer beacuse:")
+        print("This sniffer give you a warning if you are trying to reach a malware")
         print("Or a malicious hacker trying to reach/hack your computer")
         print("So, thank you for using us and hope we will do a great job")
 
-    # return (IPsrc, PORTsrc, IPdst, PORTdst)
-    def make_stemp(self, packet):
+    # this function inspects the packet and
+    # extracts featurs vector from them
+    def make_stemp(pkt):
+
+        if IP in pkt:
+            ip_src = pkt[IP].src
+            ip_dst = pkt[IP].dst
+            # protocol = pkt[IP].proto
+
+        # port_src = pkt[protocol].sport
+        if TCP in pkt:
+            port_src = pkt[TCP].sport
+            port_dst = pkt[TCP].dport
+            protocol = "TCP"
+
+        elif UDP in pkt:
+            port_src = pkt[UDP].sport
+            port_dst = pkt[UDP].dport
+            protocol = "UDP"
+
+        elif ICMP in pkt:
+            port_src = pkt[ICMP].sport
+            port_dst = pkt[ICMP].dport
+            protocol = "ICMP"
+
+        else:
+            return None  # if not tcp or udp
+        return (ip_src, port_src, ip_dst, port_dst, protocol)
 
     # This function will give us the next packet to check if correct
     def get_and_update_next_packet(self):
-        self.current_packet = S.sniff(count=1)
+        self.current_packet = sniff(count=1)
         packet = self.current_packet
         (IPsrc, PORTsrc, IPdst, PORTdst, proto) = make_stemp(packet)
         stemp = {IPsrc, PORTsrc, IPdst, PORTdst, proto}
