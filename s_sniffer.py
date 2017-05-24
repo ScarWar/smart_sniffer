0a066e725f62635aae2cf1b7d3626e86ecb44d16
tomersamara <tomersamara@mail.tau.ac.il>
2017-05-25 01:04:23 +0300

    Add The new sniffer and fix it

--- s_sniffer.py
+++ s_sniffer.py
@@ -1,115 +1,63 @@
-from scapy.all import *
+import threading
+from scapy import *
+import session_class
 
 FILE_LOGGER = "./Ssniffer_logger.log"
 
+class Sniffer(object):
+	"""
+	The Ssniffer smart sniffer that will
+	alert if we got a malware
+	"""
 
-# The Flow:
-# 1. need to sniff object
-# 2. extract features
-# 3. use ML
-# 4. 	bad:
-#			-- show result, warning and continue
-# 		good:
-#			-- continue
-#
-# Quesiton need to solve
-# 1. how to extracet features from pakets
+	# This function is to inform that we start using our
+	# Ssniffer to check what is good and bad
+	def __init__(self, ourIP):
+		self.file = open(FILE_LOGGER, "w")
+		self.ourIP = ourIP
+		self.current_packet = None
+		self.sessions = {}
+
+		# Sexy information about our Ssinffer
+		print "Hello everyone this is the Ssniffer"
+		print "This sinffer is much better then any other sniffer beacuse:"
+		print "These sniffer give you a warning if you are trying to reach a malware"
+		print "Or a malicious hacker trying to reach/hack your computer"
+		print "So, thank you for using us and hope we will do a great job"
 
 
-class S_sniffer(object):
-    """
-    The Ssniffer smart sniffer that will
-    alert if we got a malware
-    """
-
-    # This function is to inform that we start using our
-    # Ssniffer to check what is good and bad
-    def __init__(self, our_ip):
-        self.file = open(FILE_LOGGER, "w")
-        self.our_ip = our_ip
-        self.sessions = {}
-        self.current_packet = None
-        print("Hello everyone this is the Ssniffer")
-        print("This sinffer is much better then any other sniffer beacuse:")
-        print("These sniffer give you a warning if you are trying to reach a malware")
-        print("Or a malicious hacker trying to reach/hack your computer")
-        print("So, thank you for using us and hope we will do a great job")
-
-    # this function inspects the packet and
-    # extracts featurs vector from them
-    def make_stemp(pkt):
+    def make_stemp(self,pkt):
 
         if IP in pkt:
-            ip_src = pkt[IP].src
-            ip_dst = pkt[IP].dst
-            # protocol = pkt[IP].proto
+            ip_send  = pkt[IP].src
+            ip_rec   = pkt[IP].dst
 
-        # port_src = pkt[protocol].sport
         if TCP in pkt:
-            port_src = pkt[TCP].sport
-            port_dst = pkt[TCP].dport
-            protocol = "TCP"
-
+            port_send = pkt[TCP].sport
+            port_rec  = pkt[TCP].dport
+            protocol  = "TCP"
         elif UDP in pkt:
-            port_src = pkt[UDP].sport
-            port_dst = pkt[UDP].dport
-            protocol = "UDP"
-
+            port_send = pkt[UDP].sport
+            port_rec  = pkt[UDP].dport
+            protocol  = "UDP"
         elif ICMP in pkt:
-            port_src = pkt[ICMP].sport
-            port_dst = pkt[ICMP].dport
-            protocol = "ICMP"
-
+            port_send = pkt[ICMP].sport
+            port_rec  = pkt[ICMP].dport
+            protocol  = "ICMP"
         else:
-            return None  # if not tcp or udp
-        return (ip_src, port_src, ip_dst, port_dst, protocol)
+            return None # if not TCP or UDP or ICMP
 
-    # This function will give us the next packet to check if correct
-    def get_and_update_next_packet(self):
-        self.current_packet = S.sniff(count=1)
-        packet = self.current_packet
-        (IPsrc, PORTsrc, IPdst, PORTdst, proto) = make_stemp(packet)
-        stemp = {IPsrc, PORTsrc, IPdst, PORTdst, proto}
+        return (ip_send, ip_rec, port_send, port_rec, protocol)
 
+    def set_session(self, packet, stemp):
+        self.sessions[stemp] = session_class.session(packet, stemp)
+
+	# This function will give us the next packet to check if correct
+    def update_next_packet(self):
+		packet = sniff(filter="tcp.len > 0", count = 1)
+		(ip_send, ip_rec, port_send, port_rec) = make_stemp(packet)
+		stemp = set([ip_send, ip_rec, port_send, port_rec])
         if self.sessions.get(stemp) is None:
-            self.sessions[stemp] = {}
-            if self.ourIP == IPsrc:
-                self.sessions[stemp]["income"] = packet
-                return self.sessions[stemp]["income"]
-            else:
-                self.sessions[stemp]["outcome"] = packet
-                return self.sessions[stemp]["outcome"]
+            threading.Thread(target=(self.set_sessions()), args={packet, stemp})
         else:
-            if self.ourIP == IPsrc:
-                if self.sessions[stemp].get("income") is None:
-                    self.sessions[stemp]["income"] = packet
-                else:
-                    self.sessions[stemp]["income"] += packet
-                    return self.sessions[stemp]["income"]
-            else:
-                if self.sessions[stemp].get("outcome") is None:
-                    self.sessions[stemp]["outcome"] = packet
-                else:
-                    self.sessions[stemp]["outcome"] += packet
-                    return self.sessions[stemp]["outcome"]
-
-    def Warning_Mode(self, packet):
-
-
-def main():
-    better_sniffer = S_sniffer()
-
-    while True:
-        better_sniffer.update_next_packet()
-        if ML_detection() is True:
-            better_sniffer.Warning_Mode()
-        else:
-            continue
-
-
-# This function will detect if the packet is malicious or not
-# It will get thier output after genereta a classifier
-# return true if the packet seems cool
-# return false otherwise
-def ML_detection(packet):
-    return True
+            threading.Thread(target=self.sessions[stemp].update_session, args={packet}).start()
